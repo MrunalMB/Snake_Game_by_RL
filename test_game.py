96257e908512b23d913ca35e0ce2fc04e3a9392c
MrunalMB <107421478+MrunalMB@users.noreply.github.com>
2026-04-07 22:19:46 +0530

    Create test_game.py

---
+++ test_game.py
@@ -0,0 +1,12 @@
+from snake_game_ai import SnakeGameAI
+
+game = SnakeGameAI()
+
+while True:
+    action = [1, 0, 0]  # always go straight
+    reward, game_over, score = game.play_step(action)
+
+    if game_over:
+        break
+
+print("Final Score:", score)

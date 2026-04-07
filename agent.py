1af6f6bfdaa2878c9cf3ce99e2d9ac356530b6c4
MrunalMB <107421478+MrunalMB@users.noreply.github.com>
2026-04-07 22:16:33 +0530

    Create agent.py
    created agent.py file

---
+++ agent.py
@@ -0,0 +1,100 @@
+import torch
+from collections import deque
+import random
+import numpy as np
+from snake_game_ai import Direction, Point
+from model import Linear_QNet, QTrainer
+
+
+class Agent:
+
+    def __init__(self):
+        self.n_games = 0
+        self.epsilon = 0  # exploration factor
+        self.gamma = 0.9  # discount rate
+
+        # memory
+        self.memory = deque(maxlen=100_000) #stores experience
+
+        # model
+        self.model = Linear_QNet(11, 256, 3) #predicts actions
+        self.trainer = QTrainer(self.model, lr=0.001, gamma=self.gamma) #updates model
+
+    def get_state(self, game):
+        head = game.snake[0]
+
+        point_l = Point(head.x - 20, head.y)
+        point_r = Point(head.x + 20, head.y)
+        point_u = Point(head.x, head.y - 20)
+        point_d = Point(head.x, head.y + 20)
+
+        dir_l = game.direction == Direction.LEFT
+        dir_r = game.direction == Direction.RIGHT
+        dir_u = game.direction == Direction.UP
+        dir_d = game.direction == Direction.DOWN
+
+        state = [
+            # Danger straight
+            (dir_r and game.is_collision(point_r)) or
+            (dir_l and game.is_collision(point_l)) or
+            (dir_u and game.is_collision(point_u)) or
+            (dir_d and game.is_collision(point_d)),
+
+            # Danger right
+            (dir_u and game.is_collision(point_r)) or
+            (dir_d and game.is_collision(point_l)) or
+            (dir_l and game.is_collision(point_u)) or
+            (dir_r and game.is_collision(point_d)),
+
+            # Danger left
+            (dir_d and game.is_collision(point_r)) or
+            (dir_u and game.is_collision(point_l)) or
+            (dir_r and game.is_collision(point_u)) or
+            (dir_l and game.is_collision(point_d)),
+
+            # Move direction
+            dir_l,
+            dir_r,
+            dir_u,
+            dir_d,
+
+            # Food location
+            game.food.x < game.head.x,
+            game.food.x > game.head.x,
+            game.food.y < game.head.y,
+            game.food.y > game.head.y
+        ]
+
+        return np.array(state, dtype=int)
+
+    def remember(self, state, action, reward, next_state, done):
+        self.memory.append((state, action, reward, next_state, done))
+
+    def train_short_memory(self, state, action, reward, next_state, done):
+        self.trainer.train_step(state, action, reward, next_state, done)
+
+    def train_long_memory(self):
+        if len(self.memory) > 1000:
+            mini_sample = random.sample(self.memory, 1000)
+        else:
+            mini_sample = self.memory
+
+        states, actions, rewards, next_states, dones = zip(*mini_sample)
+        self.trainer.train_step(states, actions, rewards, next_states, dones)
+
+    def get_action(self, state):
+        # exploration vs exploitation
+        self.epsilon = 80 - self.n_games
+
+        final_move = [0, 0, 0]
+
+        if random.randint(0, 200) < self.epsilon:
+            move = random.randint(0, 2)
+            final_move[move] = 1
+        else:
+            state0 = torch.tensor(state, dtype=torch.float)
+            prediction = self.model(state0)
+            move = torch.argmax(prediction).item()
+            final_move[move] = 1
+
+        return final_move
